7d02f8797a90d8220bfac04a208630dbcc77498a
Artoym Shafikov <Artoym0912@gmail.com>
2020-05-11 20:50:22 +0300

    1.0.3 Added Database and small improvements

--- app/src/main/java/space/shafya/notes/Models/User.java
+++ app/src/main/java/space/shafya/notes/Models/User.java
@@ -1,37 +1,21 @@
 package space.shafya.notes.Models;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
+// [START blog_user_class]
+@IgnoreExtraProperties
 public class User {
-    private String name, email, userId;
 
-    public User() {}
+    public String username;
+    public String email;
 
-    public User(String userId, String name, String email) {
-        this.userId = userId;
-        this.name = name;
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
         this.email = email;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
--- app/src/main/java/space/shafya/notes/Sign.java
+++ app/src/main/java/space/shafya/notes/Sign.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.DialogInterface;
 
 import android.content.Intent;
@@ -19,15 +18,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -37,12 +38,15 @@ import space.shafya.notes.Models.User;
 public class Sign extends AppCompatActivity {
 
     private static final String TAG = "Sign";
+    public static final String EXTRA_POST_KEY = "post_key";
+
     Button btnSignIn, btnRegister;
     FirebaseAuth auth;
     FirebaseDatabase db;
     DatabaseReference users;
     RelativeLayout root;
     ProgressBar progressBar;
+    private DatabaseReference mDatabase;
 
 
     @Override
@@ -55,6 +59,8 @@ public class Sign extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         users = db.getReference("Users");
         root = findViewById(R.id.root_element);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -113,7 +119,6 @@ public class Sign extends AppCompatActivity {
                     public void onSuccess(AuthResult authResult) {
                         Log.d(TAG, "Вход выполнен");
                         progressBar.setElevation(1);
-                        progressBar.setVisibility(ProgressBar.INVISIBLE);
                         finish();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -121,7 +126,6 @@ public class Sign extends AppCompatActivity {
             public void onFailure(@NonNull Exception e) {
                 btnSignIn.setText(R.string.btnSignIn);
                 progressBar.setElevation(1);
-                progressBar.setVisibility(ProgressBar.INVISIBLE);
                 Log.d(TAG, "Ошибка при входе");
                 Snackbar.make(root, "Произошла ошибка при авторизации", Snackbar.LENGTH_LONG).show();
             }
@@ -178,32 +182,84 @@ public class Sign extends AppCompatActivity {
                     return;
                 }
 
-                // Регистрация пользователя
-                auth.createUserWithEmailAndPassword(email.getText().toString().toLowerCase(), pass.getText().toString())
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                User user = new User();
-                                user.setUserId(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                                user.setEmail(email.getText().toString().toLowerCase());
-                                user.setName(name.getText().toString());
+                String emaill = email.getText().toString();
+                String password = pass.getText().toString();
 
-                                Log.d(TAG, "ПЕРЕДАЧА ДАННЫХ");
-                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user)
-                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void aVoid) {
-                                                Snackbar.make(root, "Пользователь добавлен", Snackbar.LENGTH_LONG).show();
-                                            }
-                                        }).addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Snackbar.make(root, "Произошла ошибка при регистрации", Snackbar.LENGTH_LONG).setTextColor(Color.parseColor("#33AAAAAA"))
-                                                .setTextColor(Color.parseColor("#082654")).show();
+                // Регистрация пользователя
+//                auth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString()).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        if(e.getMessage().toString().equals("FirebaseAuthUserCollisionException")){
+//                            Log.d(TAG, "Почта используется");
+//                            Toast toast = Toast.makeText(Sign.this, "Почта используется", Toast.LENGTH_SHORT);
+//                            toast.setGravity(Gravity.BOTTOM, 0, 450);
+//                            toast.show();
+//                            return;
+//                        }else if(e.getMessage().toString().equals("FirebaseAuthInvalidCredentialsException")){
+//                            Log.d(TAG, "Некорректная почта");
+//                            Toast toast = Toast.makeText(Sign.this, "Некорректная почта", Toast.LENGTH_SHORT);
+//                            toast.setGravity(Gravity.BOTTOM, 0, 450);
+//                            toast.show();
+//                        }
+//                    }
+//                });
+                    auth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                        .addOnCompleteListener(Sign.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Аккаунт создан успешно");
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.d(TAG, "Ошибка в регистрации", task.getException());
+                                    if(task.getException().toString().equals("com.google.firebase.auth.FirebaseAuthUserCollisionException: The email address is already in use by another account.")){
+                                        Log.d(TAG, "Почта используется");
+                                        Toast toast = Toast.makeText(Sign.this, "Почта используется", Toast.LENGTH_SHORT);
+                                        toast.setGravity(Gravity.BOTTOM, 0, 450);
+                                        toast.show();
+                                        return;
+                                    }else if(task.getException().toString().equals("com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The email address is badly formatted.")){
+                                        Log.d(TAG, "Некорректная почта");
+                                        Toast toast = Toast.makeText(Sign.this, "Некорректная почта", Toast.LENGTH_SHORT);
+                                        toast.setGravity(Gravity.BOTTOM, 0, 450);
+                                        toast.show();
+                                        return;
+                                    }else {
+                                        Log.d(TAG, "Ошибка в создание аккаунта");
+                                        Toast toast = Toast.makeText(Sign.this, "Проверьте соединение с интернетом", Toast.LENGTH_SHORT);
+                                        toast.setGravity(Gravity.BOTTOM, 0, 450);
+                                        toast.show();
+                                        //return;
                                     }
-                                });
+
+                                }
+
                             }
-                        });
+                        }).addOnCompleteListener(Sign.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(ProgressBar.VISIBLE);
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }, 4000);
+                        btnSignIn.setText("");
+                        progressBar.setElevation(7);
+                        Log.d(TAG, "Пользователь создан:" + task.isSuccessful());
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser(), name.getText().toString());
+
+                        } else {
+                            progressBar.setElevation(1);
+                            btnSignIn.setText(R.string.btnSignIn);
+                        }
+                    }
+                });
 
 
             }
@@ -212,9 +268,29 @@ public class Sign extends AppCompatActivity {
         dialog.show();
     }
 
+    private void onAuthSuccess(FirebaseUser user, String username) {
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+        progressBar.setElevation(1);
+
+        // Go to MainActivity
+        finish();
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
 }
